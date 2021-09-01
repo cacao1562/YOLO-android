4223d65075eb36026b4b387d6f8c3f795e3b1f00
jhjun@megastudy.net <jh.jun92@gmail.com>
2021-09-02 01:15:04 +0900

    build, feat : social login setting

--- app/src/main/java/com/yolo/yolo_android/YoLoApplication.kt
+++ app/src/main/java/com/yolo/yolo_android/YoLoApplication.kt
@@ -1,18 +1,36 @@
 package com.yolo.yolo_android
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+import com.nhn.android.naverlogin.OAuthLogin
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class YoLoApplication: Application() {
-
     init {
         context = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val naverClientId = getString(R.string.naver_client_id)
+        val naverClientSecret = getString(R.string.naver_client_secret)
+        val naverClientName = getString(R.string.app_name)  // Naver App 을 통한 로그인시 노출되는 이름
+        naverOAuthLoginInstance.init(this, naverClientId, naverClientSecret, naverClientName)
+        KakaoSdk.init(this, getString(R.string.kakao_native_app_key))
+
     }
 
     companion object {
         var context: YoLoApplication? = null
             private set
+
+        val naverOAuthLoginInstance: OAuthLogin by lazy {
+            OAuthLogin.getInstance().apply {
+                setMarketLinkWorking(false)
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/yolo/yolo_android/ui/login/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.yolo.yolo_android.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nhn.android.naverlogin.OAuthLoginHandler
+import com.yolo.yolo_android.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
