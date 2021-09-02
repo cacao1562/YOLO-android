7a00693cf36643c3fe18b87a22bd34319ed17201
cacao <cacao1562@naver.com>
2021-09-02 10:58:21 +0900

    Add get image gps info

--- app/src/main/java/com/yolo/yolo_android/ui/community_upload/CommunityUploadFragment.kt
+++ app/src/main/java/com/yolo/yolo_android/ui/community_upload/CommunityUploadFragment.kt
@@ -1,13 +1,15 @@
 package com.yolo.yolo_android.ui.community_upload
 
+import android.Manifest
 import android.graphics.Rect
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.exifinterface.media.ExifInterface
 import androidx.recyclerview.widget.RecyclerView
-import com.yolo.yolo_android.R
+import com.yolo.yolo_android.*
 import com.yolo.yolo_android.base.BindingFragment
 import com.yolo.yolo_android.databinding.FragmentCommunityUploadBinding
-import com.yolo.yolo_android.dpToPx
 import gun0912.tedimagepicker.builder.TedImagePicker
 
 class CommunityUploadFragment: BindingFragment<FragmentCommunityUploadBinding>(R.layout.fragment_community_upload) {
@@ -25,8 +27,22 @@ class CommunityUploadFragment: BindingFragment<FragmentCommunityUploadBinding>(R
             setHasFixedSize(true)
             addItemDecoration(ImageSelectedDecoration(10.dpToPx()))
         }
+        if (isPermissionGranted(requireContext(), Manifest.permission.ACCESS_MEDIA_LOCATION)) {
+            requestPermission(requireActivity(), Manifest.permission.ACCESS_MEDIA_LOCATION)
+        }
         binding.ivImagePicker.setOnClickListener {
             TedImagePicker.with(requireContext()).startMultiImage {
+                it.forEach {
+                    try {
+                        val path = uri2path(requireContext(), it) ?: ""
+                        val exif = ExifInterface(path)
+                        exif.latLong?.forEach {
+                            Log.d("image gps", "value=$it")
+                        }
+                    }catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
                 imageSelectedAdapter.setData(it)
             }
         }
