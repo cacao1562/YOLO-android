86910b4999ea9ac3b194b1509f127761c591b677
jhjun@megastudy.net <jh.jun92@gmail.com>
2021-09-28 00:21:38 +0900

    Fix Home - Exception

--- app/src/main/java/com/yolo/yolo_android/ui/home_calendar/CalendarFragment.kt
+++ app/src/main/java/com/yolo/yolo_android/ui/home_calendar/CalendarFragment.kt
@@ -8,6 +8,7 @@ import com.prolificinteractive.materialcalendarview.CalendarMode
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener
 import com.yolo.yolo_android.R
 import com.yolo.yolo_android.base.BindingFragment
+import com.yolo.yolo_android.common.extensions.ToastExt.toast
 import com.yolo.yolo_android.databinding.FragmentCalendarBinding
 import com.yolo.yolo_android.ui.home_calendar.decorator.*
 import org.threeten.bp.LocalDate
@@ -17,6 +18,9 @@ class CalendarFragment : BindingFragment<FragmentCalendarBinding>(R.layout.fragm
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.ivClose.setOnClickListener { findNavController().popBackStack() }
+        binding.btnApply.setOnClickListener {
+            toast("${binding.calendarView.selectedDate?.date}")
+        }
 
         initCalendar()
     }
@@ -38,14 +42,8 @@ class CalendarFragment : BindingFragment<FragmentCalendarBinding>(R.layout.fragm
                 .setMaximumDate(CalendarDay.from(maxLocalDate()))
                 .setCalendarDisplayMode(CalendarMode.MONTHS)
                 .commit()
-
-            setOnDateChangedListener(onDateSelectedListener)
-
         }
     }
-
-    private val onDateSelectedListener =
-        OnDateSelectedListener { _, calendarDay, _ -> }
 
     companion object {
         fun newInstance(): CalendarFragment {
